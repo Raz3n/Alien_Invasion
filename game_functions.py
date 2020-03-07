a5bd9c2ac5c4fc7749cde5c4a89f12eae383c103
Nelson <nbritob@hotmail.com>
2020-03-07 15:07:46 +0000

    adds continuous movement both left and right

--- game_functions.py
+++ game_functions.py
@@ -8,12 +8,20 @@ def check_events(ship):
             sys.exit()
             
         elif event.type == pygame.KEYDOWN:
+            
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
                 
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+                
         elif event.type == pygame.KEYUP:
+            
             if event.key == pygame.K_RIGHT:
-                ship.moving_right = False      
+                ship.moving_right = False 
+                 
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False    
             
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_colour)
