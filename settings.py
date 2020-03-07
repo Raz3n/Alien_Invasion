cbd53200f0a328fdc97806a93696d58438ebb488
Nelson <nbritob@hotmail.com>
2020-03-07 15:47:38 +0000

    adds bullets to settings

--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ class Settings():
         self.bg_colour = (230, 230, 230)
         
         self.ship_speed_factor = 1.5
+        
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = 60, 60, 60
