d1cfb73e54f94b698fd73bfe6a48e4bd13d375e0
Nelson <nbritob@hotmail.com>
2020-03-09 14:33:44 +0000

    adds check aliens bottom function

--- game_functions.py
+++ game_functions.py
@@ -72,8 +72,26 @@ def change_fleet_direction(ai_settings, aliens):
     for alien in aliens.sprites():
         alien.rect.y += ai_settings.alien_drop_speed
     ai_settings.fleet_direction *= -1
+    
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
+        aliens.empty()
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+    
+        sleep(0.5)
+    else:
+        stats.game_active = False
         
-            
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+        
 def update_screen(ai_settings, screen, ship, aliens, bullets):
     screen.fill(ai_settings.bg_colour)
     for bullet in bullets.sprites():
@@ -97,18 +115,6 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     if len(aliens) == 0:
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
-        
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
-    if stats.ships_left > 0:
-        stats.ships_left -= 1
-        aliens.empty()
-        bullets.empty()
-        create_fleet(ai_settings, screen, ship, aliens)
-        ship.center_ship()
-    
-        sleep(0.5)
-    else:
-        stats.game_active = False
                 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     check_fleet_edges(ai_settings, aliens)
@@ -116,3 +122,5 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
