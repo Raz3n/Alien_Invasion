5c86f541d4afa7cf2789bcd2b5e0d7aba61399d8
Nelson <nbritob@hotmail.com>
2020-03-09 15:39:52 +0000

    adds dynamic settings and increase speed functions

--- settings.py
+++ settings.py
@@ -16,4 +16,18 @@ class Settings():
         self.alien_speed_factor = 3
         self.ship_limit = 3
         self.alien_drop_speed = 10
+        self.speedup_scale = 1.1
         self.fleet_direction = 1
+        
+        self.initialize_dynamic_settings()
+
+def initialize_dynamic_settings(self):
+    self.ship_speed_factor = 3.5
+    self.bullet_speed = 5
+    self.alien_speed_factor = 1
+    self.fleet_direction = 1
+    
+def increase_speed(self):
+    self.ship_speed_factor *= self.speedup_scale
+    self.bullet_speed *= self.speedup_scale
+    self.alien_drop_speed *= self.speedup_scale
