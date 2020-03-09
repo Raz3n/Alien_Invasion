6d81afd6ce1026e05615145b40119dce3b39b966
Nelson <nbritob@hotmail.com>
2020-03-09 15:26:27 +0000

    hides mouse cursor when game starts

--- game_functions.py
+++ game_functions.py
@@ -62,14 +62,17 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     if play_button.rect.collidepoint(mouse_x, mouse_y):
-        stats.reset_stats()
-        stats.game_active = True
+        button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+        if button_clicked and not stats.game_active:
+            pygame.mouse.set_visible(False)
+            stats.reset_stats()
+            stats.game_active = True
         
-        aliens.empty()
-        bullets.empty()
+            aliens.empty()
+            bullets.empty()
         
-        create_fleet(ai_settings, screen, ship, aliens)
-        ship.center_ship()
+            create_fleet(ai_settings, screen, ship, aliens)
+            ship.center_ship()
             
 def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
@@ -98,6 +101,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
         
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     screen_rect = screen.get_rect()
