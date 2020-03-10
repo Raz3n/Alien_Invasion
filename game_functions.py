db2efa472bc325634a82848b0cf8d9d6f934a376
Nelson <nbritob@hotmail.com>
2020-03-10 21:29:14 +0000

    adds highscore to display in game

--- game_functions.py
+++ game_functions.py
@@ -134,13 +134,21 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     
     if collisions:
-        stats.score += ai_settings.alien_points * len(aliens)
-        sb.prep_score()
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
+        check_high_score(stats, sb)
+        
     
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
+        
+def check_high_score(stats, sb):
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
                 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     check_fleet_edges(ai_settings, aliens)
--- scoreboard.py
+++ scoreboard.py
@@ -34,3 +34,4 @@ class Scoreboard():
         
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
