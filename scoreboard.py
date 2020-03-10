5bea793eac4624523198db64450764d2e1192d58
Nelson <nbritob@hotmail.com>
2020-03-10 20:32:10 +0000

    adds prep highscore function to sb

--- scoreboard.py
+++ scoreboard.py
@@ -12,6 +12,8 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 48)
         
         self.prep_score()
+        self.prep_high_score()
+
         
     def prep_score(self):
         rounded_score = int(round(self.stats.score, -1))
@@ -21,6 +23,14 @@ class Scoreboard():
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+        
+    def prep_high_score(self):
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_colour, self.ai_settings.bg_colour)
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
         
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
